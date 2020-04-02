023085e4953d1c4897ddb08c99549bd5134162a5
richard henn <49575636+herm95@users.noreply.github.com>
2020-04-02 12:04:22 -0500

    nvm

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    
     <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1">
     <title>Math Bank</title>
     <link rel="stylesheet" type="text/css" href="style.css">
